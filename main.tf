c4cd73f879b43497e833108e1b1f495ffc56c0e9
Robert Pearce <rob.pearce@fluxfederation.com>
2021-12-20 10:04:10 +1300

    -

--- main.tf
+++ main.tf
@@ -147,7 +147,7 @@ locals {
       runners_use_private_address       = !var.runners_use_private_address
       runners_request_spot_instance     = var.runners_request_spot_instance
       runners_environment_vars          = jsonencode(var.runners_environment_vars)
-      runners_pre_build_script          = var.runners_pre_build_script
+      runners_pre_build_script          = base64decode(var.runners_pre_build_script)
       runners_post_build_script         = var.runners_post_build_script
       runners_pre_clone_script          = var.runners_pre_clone_script
       runners_request_concurrency       = var.runners_request_concurrency
