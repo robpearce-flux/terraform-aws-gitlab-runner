ca86eaf30a80b9076dd55ffde410a870d7b1ee97
robpearce-flux <94561355+robpearce-flux@users.noreply.github.com>
2023-04-21 09:21:00 +1200

    fix outputs to be data source

--- outputs.tf
+++ outputs.tf
@@ -25,12 +25,12 @@ output "runner_agent_role_name" {
 
 output "runner_role_arn" {
   description = "ARN of the role used for the docker machine runners."
-  value       = aws_iam_role.docker_machine.arn
+  value       = data.aws_iam_role.docker_machine.arn
 }
 
 output "runner_role_name" {
   description = "Name of the role used for the docker machine runners."
-  value       = aws_iam_role.docker_machine.name
+  value       = data.aws_iam_role.docker_machine.name
 }
 
 output "runner_agent_sg_id" {
